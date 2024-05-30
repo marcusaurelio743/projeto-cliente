51f1accae771d6fe01cb52ca48ff9f630b443740
marcus aurelio <marcusaurelio743@gmail.com>
2024-05-30 18:17:26 -0300

    criando entidade e adicionando plugin do lombook

---
+++ clientes/src/main/java/clientes/model/entity/Cliente.java
@@ -0,0 +1,26 @@
+package clientes.model.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false, length = 200)
+	private String nome;
+	@Column(nullable = false, length = 100)
+	private String cpf;
+	private LocalDate dataCadastro;
+	
+	
+}
