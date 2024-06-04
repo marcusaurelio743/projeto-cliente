30ca57ee2c54e4e654abc21f6993f392cc84ea4b
marcus aurelio <marcusaurelio743@gmail.com>
2024-06-03 23:00:45 -0300

    salvando um cliente no banco como teste

--- clientes/src/main/java/clientes/ClientesApplication.java
+++ clientes/src/main/java/clientes/ClientesApplication.java
@@ -1,10 +1,24 @@
 package clientes;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import clientes.model.entity.Cliente;
+import clientes.repository.ClienteRepository;
 
 @SpringBootApplication
 public class ClientesApplication {
+	
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repository) {
+		return args-> {
+			Cliente cliente = Cliente.builder().cpf("1234567788").nome("José dos santos").build();
+			repository.save(cliente);
+		};
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientesApplication.class, args);
--- clientes/src/main/java/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/clientes/model/entity/Cliente.java
@@ -7,11 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +28,11 @@ public class Cliente {
 	@Column(nullable = false, length = 100)
 	private String cpf;
 	private LocalDate dataCadastro;
+	
+	@PrePersist
+	public void prePersist() {
+		setDataCadastro(LocalDate.now());
+	}
 	
 	
 }
